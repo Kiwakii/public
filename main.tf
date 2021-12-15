a60c6da436d9ed52b6301f57773bc538750827b1
Kiwakii <bright_bmp@hotmail.com>
2021-12-15 13:21:33 -0500

    trying full path

--- main.tf
+++ main.tf
@@ -29,12 +29,12 @@ provider "aws" {
 resource "aws_s3_bucket_object" "objtf" {
   bucket = "experiment-terraform"
   key    = "aws_s3_bucket_object.objtf"
-  source = "index.html"
+  source = "C:/Users/brigh/OneDrive/Documents/Projects/Dow_Jones_AWS/terraform"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("index.html")
+  etag = filemd5("C:/Users/brigh/OneDrive/Documents/Projects/Dow_Jones_AWS/terraform")
 }
 
 resource "random_pet" "sg" {}
