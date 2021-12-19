85d71e6d7dc2dceeb5ce16d9fa626a9e0df433af
Kiwakii <bright_bmp@hotmail.com>
2021-12-18 20:27:51 -0500

    added index_a

--- main.tf
+++ main.tf
@@ -35,7 +35,16 @@ resource "aws_s3_bucket_object" "objtf" {
   # etag = "${md5(file("path/to/file"))}"
   etag = filemd5("index.html")
 }
+resource "aws_s3_bucket_object" "objtf" {
+  bucket = "experiment-terraform"
+  key    = "aws_s3_bucket_object_a.objtf"
+  source = "index_a.html"
 
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("index_a.html")
+}
 
 resource "random_pet" "sg" {}
 
